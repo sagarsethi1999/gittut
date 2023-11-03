dd84e368d37a89528a1a94d82a14d75c8213a572
sagarsethi1999 <sagarsethiputulu@gmail.com>
2023-11-03 19:53:33 +0530

    get elements by tag name task

--- demo.js
+++ demo.js
@@ -1,13 +1,22 @@
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[2].style.backgroundColor='green';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[2].style.backgroundColor='green';
 
-for(var i=0;i < items.length;i++){
-    items[i].style.fontWeight = 'bold';
+// for(var i=0;i < items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[2].style.backgroundColor='green';
+li[4].style.fontWeight='bold'
+
+for(var i=0;i < li.length;i++){
+    li[i].style.fontWeight = 'bold';
 }
-
 
 
 

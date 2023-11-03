1f58b3493e4b6c25a53f13f2ce5faa7948edb51f
sagarsethi1999 <sagarsethiputulu@gmail.com>
2023-11-03 19:35:13 +0530

    get elements by calss name task

--- demo.js
+++ demo.js
@@ -1,15 +1,17 @@
 
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[2].style.backgroundColor='green';
 
-console.log(document.URL);
-console.log(document.title);
-var header=document.getElementById("header-title");
-
-console.log(header);
-header.style.borderBottom="solid 3px black";
+for(var i=0;i < items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
 
 
 
-var legend=document.getElementById('legend');
-console.log(legend);
-legend.style.fontWeight='bold';
-legend.style.color='green';
+
+
+
+
+

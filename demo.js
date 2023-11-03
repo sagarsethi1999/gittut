567398d12f2cc431d80e5a32450e7b0d09037bc8
sagarsethi1999 <sagarsethiputulu@gmail.com>
2023-11-03 20:20:50 +0530

    querry selector task

--- demo.js
+++ demo.js
@@ -1,22 +1,17 @@
+var  secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+var  thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display="none";
 
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[2].style.backgroundColor='green';
 
-// for(var i=0;i < items.length;i++){
-//     items[i].style.fontWeight = 'bold';
-// }
+//Queryselectorall
 
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[2].style.backgroundColor='green';
-li[4].style.fontWeight='bold'
-
-for(var i=0;i < li.length;i++){
-    li[i].style.fontWeight = 'bold';
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i = 0;i < odd.length;i++)
+{
+   odd[i].style.backgroundColor = "lightgreen";
 }
+
 
 
 

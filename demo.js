9aa53a69527e19ed8333a05f7d3c180b87929775
sagarsethi1999 <sagarsethiputulu@gmail.com>
2023-11-03 18:45:48 +0530

    started with the DOM task

--- demo.js
+++ demo.js
@@ -1,3 +1,15 @@
 
 
-console.log("sagar sethi");
+console.log(document.URL);
+console.log(document.title);
+var header=document.getElementById("header-title");
+
+console.log(header);
+header.style.borderBottom="solid 3px black";
+
+
+
+var legend=document.getElementById('legend');
+console.log(legend);
+legend.style.fontWeight='bold';
+legend.style.color='green';
